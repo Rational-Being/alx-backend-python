bd034b2a26a928bda827f169e99337e827583caf
Rational-Being <overcomer021@gmail.com>
2024-05-02 18:28:34 +0100

    pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,13 +3,11 @@
 type annoted function that a string and a float as arguments and 
 returns a tuple
 """
-
-
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    retunrs a tuple when pass a stirng and float
+    retunrs a tuple when pass a stirng and float as arguments
     """
     return (k, float(v**2))
