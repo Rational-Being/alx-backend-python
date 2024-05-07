149273a7fe785f079c08507ecfcb2d6cd1189ce9
Rational-Being <overcomer021@gmail.com>
2024-05-07 18:44:30 +0100

    pycodestyle

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,8 @@ coroutine collects 10 random numbers, then returns 10 random numbers
 
 from typing import List
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = __import__("0-async_generator").async_generator
+
 
 async def async_comprehension() -> List[float]:
     """
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,10 +7,9 @@ import asyncio
 
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
-async def measure_runtime() -> float:
-    """
 
-    """
+async def measure_runtime() -> float:
+    """ """
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for i in range(4)))
     finish = time.time() - start_time
