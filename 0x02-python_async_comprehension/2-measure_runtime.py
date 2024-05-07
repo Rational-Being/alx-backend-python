c97f110a0cb20e0be5e3485c0b768753c94e87ec
Rational-Being <overcomer021@gmail.com>
2024-05-07 18:50:43 +0100

    pycodestyle

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,7 +10,7 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ 
+    """
     runtime for four parallel comprehension
     """
     start_time = time.time()
